daa9e650226cdb1401fac92df11000c83d450f08
Sandip Gyawali <sandipgyawali10@gmail.com>
2026-05-08 20:02:59 +0545

    sidebar update

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -313,7 +313,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       className={cn(
         "relative flex w-full flex-1 flex-col bg-background",
-        "md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ms-2 md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ms-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm/5",
+        "md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ms-2 md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ms-0 md:peer-data-[variant=inset]:rounded-lg border",
         className,
       )}
       data-slot="sidebar-inset"
@@ -521,7 +521,11 @@ function SidebarMenuButton({
   const { isMobile, state } = useSidebar();
 
   const defaultProps = {
-    className: cn(sidebarMenuButtonVariants({ size, variant }), className),
+    className: cn(
+      sidebarMenuButtonVariants({ size, variant }),
+      className,
+      "!rounded-sm",
+    ),
     "data-active": isActive,
     "data-sidebar": "menu-button",
     "data-size": size,
